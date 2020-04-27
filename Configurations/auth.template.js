7cf3b538b26ca7a6bba17ce03f9a4b1b443bdf2a
Anurag374-gill <61975299+Anurag374-gill@users.noreply.github.com>
2020-04-27 12:27:24 +0530

    Update auth.template.js

--- Configurations/auth.template.js
+++ Configurations/auth.template.js
@@ -1,8 +1,8 @@
 module.exports = {
 	discord: {
-		clientID: "",
-		clientSecret: "",
-		clientToken: "",
+		clientID: "691300088026365963",
+		clientSecret: "9YOmID_QXRIklQueZPsvE2B5FHGdZeCL",
+		clientToken: "NjkxMzAwMDg4MDI2MzY1OTYz.XqaAhQ.HZIogFz7BEU-1zeZaFIzQoiaPf0",
 	},
 	tokens: {
 		discordBotsOrg: "",
